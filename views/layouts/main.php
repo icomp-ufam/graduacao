13899c5d96b921a546860c6368701d6c4eb896fe
Gabriel Gama <gabrielgamagga@gmail.com>
2015-10-21 21:25:48 -0400

    restaurando a aplicacao

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Controle de Atividades Acadêmicas',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
