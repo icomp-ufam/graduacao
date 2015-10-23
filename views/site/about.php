338dd45d52276e9dde9ffe8bf74a333089d0b8d9
zottoz <zottozbr@gmail.com>
2015-10-23 01:23:46 +0000

    atualizando about page

--- views/site/about.php
+++ views/site/about.php
@@ -10,9 +10,23 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Bem vindo: </h1> 
+                    <h2>
+                        <?php
+                            echo Yii::$app->user->identity->name
+                        ?>
+                    </h2>
+                <br>
+                <h1>Email: </h1> 
+                    <h2>
+                        <?php
+                            echo Yii::$app->user->identity->email
+                        ?>
+                    </h2>
+            </div>
+        </div>
+    </div>
 </div>
