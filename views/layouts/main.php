f719545a1ad7e2eaaa81a9e41a06ca10308d5401
zottoz <zottozbr@gmail.com>
2015-10-27 21:09:54 -0400

    removi o tema que nao deu certo...

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -7,12 +7,12 @@ use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\bootstrap\Dropdown;
+
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
-//AppAsset::register($this);
+AppAsset::register($this);
 ?>
-
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
@@ -22,135 +22,69 @@ use app\assets\AppAsset;
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-
-
-    <!-- Le styles -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-
-      @media (max-width: 980px) {
-        /* Enable use of floated navbar text */
-        .navbar-text.pull-right {
-          float: none;
-          padding-left: 5px;
-          padding-right: 5px;
-        }
-      }
-    </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="assets/ico/favicon.png">
-
 </head>
-
 <body>
 <?php $this->beginBody() ?>
 
 <div class="wrap">
     
+    
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'Atividades Complementares',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Cursos', 'url' => ['/curso/index']],
+            ['label' => 'Calendario', 'url' => ['/periodo/index']],
+            ['label' => 'Usuarios', 'url' => ['/usuario/index']],
+            ['label' => '[ ]', 'url' => ['#']],            
+            //['label' => 'About', 'url' => ['/site/about']],
+            //['label' => 'Contact', 'url' => ['/site/contact']],
+            Yii::$app->user->isGuest ? (
+                ['label' => 'Login', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->name . ')',
+                    ['class' => 'btn btn-link']
+                )
+                . Html::endForm()
+                . '</li>'
+            )
+        ],
+    ]);
+    NavBar::end();
+    ?>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container-fluid">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="#">Project name</a>
-          <div class="nav-collapse collapse">
-            <p class="navbar-text pull-right">
-              Logged in as 
-               
-                    <?php
-                        if( !Yii::$app->user->isGuest )
-                        {
-                            echo "<a href='?r=site/logout'>". "( Sair ) " . Yii::$app->user->identity->name . "</a>" ;
-                        }
-                    ?>
-               
-            </p>
-            <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-      </div>
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= $content ?>
     </div>
-
-
-<div class="container-fluid">
-
-    <div class="row-fluid">
-
-        <div class="span3">
-          <div class="well sidebar-nav">
-            <ul class="nav nav-list">
-              
-              <li class="nav-header">Usuario</li>
-              <li><a href="?r=usuario/create">Novo</a></li>
-              <li><a href="?r=usuario">Lista</a></li>
-
-              <li class="nav-header">Periodo</li>
-              <li><a href="?r=periodo/create">Novo</a></li>
-              <li><a href="?r=periodo">Listar</a></li>
-
-              <li class="nav-header">Curso</li>
-              <li><a href="?r=curso/create">Novo</a></li>
-              <li><a href="?r=curso">Listar</a></li>
-
-            </ul>
-          </div><!--/.well -->
-        </div><!--/span 3-->
-        
-        <div class="span9">
-          <div class="hero-unit">
-                <div class="container">
-                  
-                    <?= $content ?>
-
-                </div>
-          </div>
-        </div><!--/span 9-->
-
-    </div><!--/row-->
-
-</div><!--/.fluid-container-->
+</div>
 
 <footer class="footer">
-    <p class="pull-right"><?= Yii::powered() ?></p>
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
 </footer>
 
 
 <?php $this->endBody() ?>
-
 </body>
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
-</html>
+<!-- Scripts -->
 
+</html>
 <?php $this->endPage() ?>
