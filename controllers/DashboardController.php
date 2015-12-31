8995bff23be5a70db05a12c4e48d462d2fb9e2a5
zottoz <zottozbr@gmail.com>
2015-12-30 22:39:04 -0400

    dash aluno fim

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -5,6 +5,7 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\helpers\Html;
+use app\models\Solicitacao;
 
 class DashboardController extends \yii\web\Controller
 {
@@ -41,12 +42,54 @@ class DashboardController extends \yii\web\Controller
     {
         if(!Yii::$app->user->isGuest)
         {
+
             if(Yii::$app->user->identity->perfil == 'Aluno'){
-                return $this->render('dashAluno');
+                
+                $id = Yii::$app->user->identity->id ;               
+
+                $cmd = Yii::$app->db->createCommand("SELECT SUM(horasComputadas) AS soma 
+                    FROM solicitacao AS S WHERE S.atividade_id 
+                    IN (SELECT id FROM atividade WHERE grupo_id=1)
+                    AND S.status='Submetida'
+                    AND solicitante_id = $id 
+                ");
+                
+                $hsEnsino = $cmd->queryScalar();
+
+                $cmd = Yii::$app->db->createCommand("SELECT SUM(horasComputadas) AS soma 
+                    FROM solicitacao AS S WHERE S.atividade_id 
+                    IN (SELECT id FROM atividade WHERE grupo_id=2)
+                    AND S.status='Submetida'
+                    AND solicitante_id = $id 
+                ");
+                
+                $hsPesquisa = $cmd->queryScalar();
+                
+                $cmd = Yii::$app->db->createCommand("SELECT SUM(horasComputadas) AS soma 
+                    FROM solicitacao AS S WHERE S.atividade_id 
+                    IN (SELECT id FROM atividade WHERE grupo_id=3)
+                    AND S.status='Submetida'
+                    AND solicitante_id = $id 
+                ");
+                
+                $hsExtensao = $cmd->queryScalar();
+                
+                if($hsEnsino==null)     $hsEnsino   = 0 ;
+                if($hsPesquisa==null)   $hsPesquisa = 0 ;
+                if($hsExtensao==null)   $hsExtensao = 0 ;
+
+                return $this->render('dashAluno', [
+                    'horasEmEnsino'     =>  $hsEnsino, 
+                    'horasEmPesquisa'   =>  $hsPesquisa,
+                    'horasEmExtensao'   =>  $hsExtensao
+                ]);
             }
             if(Yii::$app->user->identity->perfil == 'Coordenador'){
                 return $this->render('dashCoordenador');
             }
+            if(Yii::$app->user->identity->perfil == 'Secretaria'){
+                return $this->render('dashSecretaria');
+            }            
         }
         else
         {
--- views/dashboard/dashAluno.php
+++ views/dashboard/dashAluno.php
@@ -2,6 +2,8 @@
 /* @var $this yii\web\View */
 //use gietos\yii\ionicons\Ion;
 use scotthuangzl\googlechart\GoogleChart;
+use miloschuman\highcharts\Highcharts;
+
 ?>
 
 <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
@@ -12,11 +14,11 @@ use scotthuangzl\googlechart\GoogleChart;
         <div class="thumbnail alert alert-success">
             <div class="inner">
                 <p><i class="fa fa-graduation-cap icon-4x pull-right"></i></p>
-                <h3>60<?php $horasEmEnsino ?> Horas</h3>
+                <h3><?php echo $horasEmEnsino ?> Horas</h3>
                 <h4>Ensino</h4>
             </div>
 
-            <a href="#" class="small-box-footer">Mais detalhes <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="?r=solicitacao/index" class="small-box-footer">Mais detalhes <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
@@ -24,11 +26,11 @@ use scotthuangzl\googlechart\GoogleChart;
         <div class="thumbnail alert alert-warning">
             <div class="inner">
                 <p><i class="fa fa-object-ungroup icon-4x pull-right"></i></p>
-                <h3>40<?php $horasEmPesquisa ?> Horas</h3>
+                <h3><?php echo $horasEmPesquisa ?> Horas</h3>
                 <h4>Pesquisa</h4>
             </div>
 
-            <a href="#" class="small-box-footer">Mais detalhes <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="?r=solicitacao/index" class="small-box-footer">Mais detalhes <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
@@ -36,11 +38,11 @@ use scotthuangzl\googlechart\GoogleChart;
         <div class="thumbnail alert alert-info">
             <div class="inner">
                 <p><i class="fa fa-paper-plane icon-4x pull-right"></i></p>
-                <h3>20<?php $horasEmExtensao ?> Horas</h3>
+                <h3><?php echo $horasEmExtensao ?> Horas</h3>
                 <h4>Extensão</h4>
             </div>
 
-            <a href="#" class="small-box-footer">Mais detalhes <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="?r=solicitacao/index" class="small-box-footer">Mais detalhes <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
 
@@ -48,48 +50,34 @@ use scotthuangzl\googlechart\GoogleChart;
 
 <div class="row">
     <div class="col-md-12">
-        <h3>Gráficos: </h3>
+        <h3>Gráfico: </h3>
     </div>
 </div>
 
 <div class="row">
-    <div class="col-md-6">
-        <?php
-            echo GoogleChart::widget(array('visualization' => 'PieChart',
-                'data' => array(
-                    array('Grupo', 'Horas'),
-                    array('Ensino', 60),
-                    array('Pesquisa', 40),
-                    array('Extensão', 20)
-                ),
-            'options' => array('title' => 'Distribuição por Grupo')));
-        ?>
+    <div class="col-md-4">
+    
     </div>
 
     <div class="col-md-6">
         <?php
-        echo GoogleChart::widget(array('visualization' => 'LineChart',
-            'data' => array(
-                array('Mês', 'Ensino', 'Pesquisa', 'Extensão'),
-                array('Janeiro',    5, 10, 1),
-                array('Fevereiro',  5, 5, 4),
-                array('Março',      15, 5, 5),
-                array('Abril',      10, 20, 2),
-                array('Junho',      15, 20, 10),
-                array('Julho',      10, 20, 5),
-            ),
-            'options' => array(
-                'title' => 'Horas x Meses x Grupos',
-                'titleTextStyle' => array('color' => '#FF0000'),
-                'vAxis' => array(
-                    'title' => 'Horas',
-                    'gridlines' => array(
-                        'color' => 'transparent'  //set grid line transparent
-                    )),
-                'hAxis' => array('title' => 'Meses'),
-                'curveType' => 'function', //smooth curve or not
-                'legend' => array('position' => 'bottom'),
-            )));
+
+            echo Highcharts::widget([
+                'options' => [               
+                    'title' => ['text' => 'Distribuição por Grupos'],
+               
+                    'series' => [[
+                        'type' => 'pie',
+                        'name' => 'Grupos',
+                        'data' => [
+                            ['name' => 'Ensino', 'y' => (float) $horasEmEnsino ],
+                            ['name' => 'Pesquisa', 'y' => (float) $horasEmPesquisa],
+                            ['name' => 'Extensão', 'y' => (float) $horasEmExtensao]                            
+                        ]
+                    ]]               
+                ]
+            ]);
         ?>
-    </div>
+    </div>  
+
 </div>
