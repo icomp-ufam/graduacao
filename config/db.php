c0f3585fc5ef63a24eb0655b8e8e931648a9b409
Leonardo Sales <Leonardo Sales>
2016-01-09 22:50:51 -0400

    ajustando db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql.hostinger.com.br;dbname=u454478552_icomp',
-    'username' => 'u454478552_icomp',
-    'password' => '123456',
+    'dsn' => 'mysql:host=localhost;dbname=icomp',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
