acbb5997b88586731b3f33bdc981cb00e67790e8
Leonardo Almeida <leonardo.almeida18@gmail.com>
2016-01-23 14:32:04 -0400

    adicionando db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=icomp',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=mysql.hostinger.com.br;dbname=u454478552_icomp',
+    'username' => 'u454478552_icomp',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
